2942360fe15a5ed2422b316a479cc0df00e19daa
Hunter Haugen <hunter@puppet.com>
2017-06-07 15:18:39 -0700

    Add hyperminimal to hyper

--- .hyper.js
+++ .hyper.js
@@ -108,7 +108,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    `hyperminimal`
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
